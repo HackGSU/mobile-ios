c3221251ac2938d50facb05fa40d51938044256e
saiharsha goli <saiharshagoli@c02rr78fg8wp.attlocal.net>
2016-10-02 15:55:53 -0400

    Added BATabBarController dependancy

---
+++ hackGSU/Pods/Target Support Files/BATabBarController/BATabBarController-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "BATabBar.h"
+#import "BATabBarController.h"
+#import "BATabBarItem.h"
+#import "UIColor+ColorWithHex.h"
+
+FOUNDATION_EXPORT double BATabBarControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char BATabBarControllerVersionString[];
+
