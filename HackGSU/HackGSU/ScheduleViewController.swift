58244dad1b72eb942a435e1915a35d3e45bd6df8
bob <user@Dylans-Macbook-Pro.local>
2016-10-04 16:51:39 -0400

    number of cells is now accurate for each section. Separators have been removed

--- HackGSU/HackGSU/ScheduleViewController.swift
+++ HackGSU/HackGSU/ScheduleViewController.swift
@@ -2,7 +2,7 @@
 //  ScheduleViewController.swift
 //  hackGSU
 //
-//  Created by user on 10/1/16.
+//  Created by Dylan Welch on 10/1/16.
 //  Copyright © 2016 hackGSU. All rights reserved.
 //
 
@@ -13,11 +13,17 @@ class ScheduleViewController: UITableViewController {
 
     let cellId = "cellId"
     
-    var time: [String] = ["2:00pm", "12:00am", "4:00pm"]
-    var eventTitle: [String] = ["New Announcement", "Announcement 2", "Announcement 3"]
-    var eventDescription: [String] = ["The event will take place in this location", "event description", "This event is cancelled"]
+    var time: [String] = [""]
+    var eventLocation: [String] = ["Location 1", "Location 2", "Location 3"]
     var dayOfWeekend: [String] = ["Friday", "Saturday", "Sunday"]
+    var timesForFriday: [String] = ["5:00pm", "6:00pm", "6:30pm", "7:00pm", "8:00pm", "8:30pm", "9:30pm", "10:30pm", "12:00am"]
+    var timesForSaturday: [String] = ["8:00am", "11:00am", "1:00pm", "1:30am", "2:00pm", "3:00pm", "6:00pm", "9:00pm", "12:00am"]
+    var timesForSunday: [String] = ["8:00am","9:00am","10:00am","11:30am", "12:15am"]
+    var eventDescriptionFriday: [String] = ["Early Check-in Begins / Late Registration", "Check-in", "Dinner", "Opening Ceremonies", "Hacking Begins Idea Mixer / Team Forming", "Development Workshop A", "Development Workshop B", "Development Workshop C", "Friday Midnight Madness"]
+    var eventDescriptionSaturday: [String] = ["Breakfast", "Development Workshop D", "Lunch", "TBD", "Devlopment Workshop E", "TBD", "Dinner", "Snack", "Saturday Midnight Madness"]
+    var eventDesriptionSunday: [String] = ["Breakfast", "Submit to Devpost Hacking Ends", "Hack Expo", "Finalist Demos", "Closing/Awards Ceremonies"]
     
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         var allowsSelection: Bool = false
@@ -25,14 +31,30 @@ class ScheduleViewController: UITableViewController {
         tableView.register(ScheduleTableCell.self, forCellReuseIdentifier: cellId)
         
     }
-    
+    /*
+    func scheduleForEachDay () -> [String]{
+        switch dayOfWeekend[section]{
+        case 0:
+            return timesForFriday
+            break
+        case 1:
+            return [timesForSaturday]
+            break
+        case 2:
+            return [timesForSunday]
+            break
+        }
+        
+        
+    }
+    */
     
     override func numberOfSections(in tableView: UITableView) -> Int{
         return 3
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String{
-        //given the index, return title stirng
+        //given the index, return title string
         return dayOfWeekend[section]
     }
     
@@ -40,13 +62,13 @@ class ScheduleViewController: UITableViewController {
         //given the index of section, give the number of rows (cells)
         switch section {
             case 0:
-                return 1
+                return 9
                 break
             case 1:
-                return 2
+                return 9
                 break
             case 2:
-                return 3
+                return 5
                 break
         default:
             return 0
@@ -62,16 +84,13 @@ class ScheduleViewController: UITableViewController {
         //given the index of the cell, return the cell
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ScheduleTableCell
         
-        cell.eventTitle.text = eventTitle[indexPath.row]
-        cell.eventText.text = eventDescription[indexPath.row]
+        
+        //cell.eventTitle.text = eventTitle[indexPath.row]
+
+        //cell.eventText.text = eventDescription[indexPath.row]
         
         return cell
     }
     
 }
 
-class ScheduleCell: UITableViewCell{
-    @IBOutlet weak var time: UILabel!
-    @IBOutlet weak var eventTitle: UILabel!
-    @IBOutlet weak var eventDescription: UILabel!
-}
