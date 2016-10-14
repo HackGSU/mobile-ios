a06cdba1616991d7604285fb207d5b598ad83b4b
Alex Mitchell <spartanawesome@gmail.com>
2016-10-13 23:56:51 -0400

    Fiished the collection view with all the pictures, names, and roles

---
+++ HackGSU/HackGSU/teamMember.swift
@@ -0,0 +1,9 @@
+//
+//  teamMember.swift
+//  hackGSU
+//
+//  Created by Alexander Mitchell "MitchellOfficial" on 10/12/16.
+//  Copyright © 2016 hackGSU. All rights reserved.
+//
+
+import Foundation
